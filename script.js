feb88aa078e4525eaea9e58f407b0b1ad4f14826
Ernesto <erestrada14@gmail.com>
2021-01-27 14:16:54 -0600

    create mesage

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ const message = {
     thirdPart : ['The harder you work for something, the greater you’ll feel when you achieve it.',
 'Dream bigger. Do bigger.', 'Don’t stop when you’re tired. Stop when you’re done.']
 };
+
+const createMessage = (obj) => {
+    let randomNumber1 = Math.floor(Math.random() * obj.firstPart.length);
+    let randomNumber2 = Math.floor(Math.random() * obj.secondPart.length);
+    let randomNumber3 = Math.floor(Math.random()* obj.thirdPart.length);
+    let message = `Your first advice of the day is: ' ${obj.firstPart[randomNumber1]} 'This will be a good thing to keep in mind: ' ${obj.secondPart[randomNumber2]} 'And this is your final quote of the day: ' ${obj.thirdPart[randomNumber3]}`;
+    return message;
+}
+
+console.log(createMessage(message));
