9f1232e109d34378b7c0d12ad42f1c3e797fbbf9
Ernesto <erestrada14@gmail.com>
2021-01-27 13:53:30 -0600

    first merge

---
+++ script.js
@@ -0,0 +1,11 @@
+// This is a program that display a random message every time it runs.
+
+const message = {
+    firstPart : ['Your limitation—it’s only your imagination.',
+'Push yourself, because no one else is going to do it for you.',
+'Sometimes later becomes never. Do it now.'],
+    secondPart : ['Great things never come from comfort zones.',
+'Dream it. Wish it. Do it.', 'Success doesn’t just find you. You have to go out and get it.'],
+    thirdPart : ['The harder you work for something, the greater you’ll feel when you achieve it.',
+'Dream bigger. Do bigger.', 'Don’t stop when you’re tired. Stop when you’re done.']
+};
